ca83efb1f992e17d4a72903ccf1fe62d6196ac71
WeiHsi Hu <walzerfan@yahoo.com>
2024-05-30 11:09:28 +0200

    extract quality change methods

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -9,47 +9,59 @@ class GildedRose
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
+            item.quality = reduce_quality(item)
           end
         end
       else
         if item.quality < 50
-          item.quality = item.quality + 1
+          item.quality = increase_quality(item)
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
               if item.quality < 50
-                item.quality = item.quality + 1
+                item.quality = increase_quality(item)
               end
             end
             if item.sell_in < 6
               if item.quality < 50
-                item.quality = item.quality + 1
+                item.quality = increase_quality(item)
               end
             end
           end
         end
       end
       if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
+        item.sell_in = reduce_sell_in(item)
       end
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
               if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
+                item.quality = reduce_quality(item)
               end
             end
           else
-            item.quality = item.quality - item.quality
+            item.quality = reduce_quality(item, item.quality)
           end
         else
           if item.quality < 50
-            item.quality = item.quality + 1
+            item.quality = increase_quality(item)
           end
         end
       end
     end
+  end
+
+  def reduce_sell_in(item)
+    item.sell_in - 1
+  end
+
+  def reduce_quality(item, reduce_by = 1)
+    item.quality - reduce_by
+  end
+
+  def increase_quality(item, increase_by = 1)
+    item.quality + increase_by
   end
 end
 
--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -40,6 +40,12 @@ describe GildedRose do
     expect(items[0].quality).to eq 50
   end
 
+  it "does not change sell_in value of Sulfuras" do
+    items = [Item.new("Sulfuras, Hand of Ragnaros", 3, 50)]
+    GildedRose.new(items).update_quality()
+    expect(items[0].sell_in).to eq 3
+  end
+
   it "increases Backstage passes quality by 1 if sell_in is more than 10" do
     items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 12, 22)]
     GildedRose.new(items).update_quality()
